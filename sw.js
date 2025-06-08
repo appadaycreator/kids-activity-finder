9c3abcc472956b1205beb2d321660930267475fa
techembrace <tech.em.brace.0629@gmail.com>
2025-06-08 22:42:56 +0900

    初回コミットに基づくファイルの追加

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'kids-activity-finder-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/assets/favicon-192.png',
+    '/assets/favicon-512.png',
+    '/assets/og-image.jpg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+}); 
